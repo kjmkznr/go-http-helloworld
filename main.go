eba9a9d7a4621988a4145f7eb0b472af52a0bc68
Kazunori Kojima <kjm.kznr@gmail.com>
2016-05-12 18:19:29 +0900

    Read MESSAGE env value

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	msg := os.Getenv("MESSAGE")
+	if msg == "" {
+		msg = "Hello, World."
+	}
 	log.Fatal(http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/plain")
-		w.Write([]byte("Hello, World."))
+		w.Write([]byte(msg))
 	})))
 }

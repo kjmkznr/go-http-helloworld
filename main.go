ad8514e28c7158c73677f0a7836fbee80780e139
KOJIMA Kazunori <kjm.kznr@gmail.com>
2019-09-06 04:56:03 +0000

    Add hostname

--- main.go
+++ main.go
@@ -9,10 +9,15 @@ import (
 func main() {
 	msg := os.Getenv("MESSAGE")
 	if msg == "" {
-		msg = "Hello, World."
+		msg = "Hello, World from " + getHostname()
 	}
 	log.Fatal(http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/plain")
 		w.Write([]byte(msg))
 	})))
+}
+
+func getHostname() string {
+	hostname, _ := os.Hostname()
+	return hostname
 }
